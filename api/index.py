e00d748a5a6fb374dbf903c0e3096c89322c422d
Neel678 <75207773+Neel678@users.noreply.github.com>
2025-06-20 19:48:47 +0530

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,13 +1,23 @@
-import json, datetime
-from flask import Flask, request, jsonify
+import json
+import datetime
+import requests
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
 
+# Function to add custom headers
+def add_headers(response):
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Server'] = 'Pro_Max_Futuristics'
+    response.headers['Custom-Header'] = 'Pro_Max'
+    return response
+
+# Function to get data
 def get_data():
     date = datetime.datetime.now()
     currentdt = date.strftime("%Y-%m-%d %H:%M:%S")
     data = {
-        "license_code": "Var. of Chaviv's",
+        "license_code": "GURU",
         "expiry_date": "2999-12-31 23:59:59",
         "plan": "Pos",
         "planId": 166,
@@ -22,20 +32,41 @@ def get_data():
     }
     return jsonify(data)
 
+# Route for old API
 @app.route('/api/license/<device_id>', methods=['GET', 'POST'])
 def api_old(device_id):
-    return get_data()
+    response = make_response(get_data())
+    return add_headers(response)
 
+# Route for new API
 @app.route('/api/ns/license/<device_id>', methods=['GET', 'POST'])
 def api_new(device_id):
-    return get_data()
-import requests
+    response = make_response(get_data())
+    return add_headers(response)
+
+# Route to forward requests to vyaparapp.in
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def catch_all(path):
+    url = f'https://vyaparapp.in/{path}'
     if request.method == 'GET':
-        r = requests.get('https://vyaparapp.in/' + path)
-        return r.text
+        r = requests.get(url)
+        response = make_response(r.text)
     elif request.method == 'POST':
-        r = requests.post('https://vyaparapp.in/' + path, data=request.form)
-        return r.text
+        r = requests.post(url, data=request.form)
+        response = make_response(r.text)
+    return add_headers(response)
+
+# Error handling
+@app.errorhandler(404)
+def page_not_found(e):
+    response = make_response(jsonify(error="Not Found"), 404)
+    return add_headers(response)
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    response = make_response(jsonify(error="Internal Server Error"), 500)
+    return add_headers(response)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
